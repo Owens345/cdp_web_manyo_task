7d6f51a03b64d60b507922bca56e4d49174000b8
Owens345 <owensomwanda@gmail.com>
2026-04-29 12:39:02 +0300

    Step3: Add deadline/priority/status, sort, search, index, tests

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,5 +2,24 @@ FactoryBot.define do
   factory :task do
     title { 'Document preparation' }
     content { 'Create a proposal.' }
+    deadline_on { Date.today }
+    priority { :medium }
+    status { :not_started }
+  end
+
+  factory :second_task, class: Task do
+    title { 'second_task' }
+    content { 'Content 2' }
+    deadline_on { '2022-02-17' }
+    priority { :high }
+    status { :in_progress }
+  end
+
+  factory :third_task, class: Task do
+    title { 'third_task' }
+    content { 'Content 3' }
+    deadline_on { '2022-02-16' }
+    priority { :low }
+    status { :completed }
   end
 end
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe 'Task management function', type: :system do
         visit new_task_path
         fill_in 'タイトル', with: 'Document preparation'
         fill_in '内容', with: 'Create a proposal.'
+        find('input[name="task[deadline_on]"]').set(Date.today)
+        select '中', from: 'task[priority]'
+        select '未着手', from: 'task[status]'
         click_button '登録する'
         expect(page).to have_content 'Document preparation'
       end
@@ -15,9 +18,9 @@ RSpec.describe 'Task management function', type: :system do
   end
 
   describe 'List display function' do
-    let!(:first_task) { FactoryBot.create(:task, title: 'first_task', created_at: '2022-02-18') }
-    let!(:second_task) { FactoryBot.create(:task, title: 'second_task', created_at: '2022-02-17') }
-    let!(:third_task) { FactoryBot.create(:task, title: 'third_task', created_at: '2022-02-16') }
+    let!(:first_task)  { FactoryBot.create(:task, title: 'first_task',  deadline_on: '2022-02-18', priority: :medium, status: :not_started, created_at: '2022-02-18') }
+    let!(:second_task) { FactoryBot.create(:second_task, title: 'second_task', deadline_on: '2022-02-17', priority: :high, status: :in_progress, created_at: '2022-02-17') }
+    let!(:third_task)  { FactoryBot.create(:third_task, title: 'third_task',  deadline_on: '2022-02-16', priority: :low,  status: :completed,   created_at: '2022-02-16') }
 
     before do
       visit tasks_path
@@ -38,6 +41,63 @@ RSpec.describe 'Task management function', type: :system do
         visit tasks_path
         task_list = all('tbody tr')
         expect(task_list[0]).to have_content 'newest_task'
+      end
+    end
+
+    describe 'Sort function' do
+      context 'If you click on the link End Deadline' do
+        it 'A list of tasks sorted in ascending order of due date is displayed.' do
+          click_link '終了期限'
+          sleep 0.2
+          task_list = all('tbody tr')
+          expect(task_list[0]).to have_content 'third_task'
+          expect(task_list[1]).to have_content 'second_task'
+          expect(task_list[2]).to have_content 'first_task'
+        end
+      end
+
+      context 'If you click on the link Priority' do
+        it 'A list of tasks sorted by priority is displayed' do
+          click_link '優先度'
+          sleep 0.2
+          task_list = all('tbody tr')
+          expect(task_list[0]).to have_content 'second_task'
+          expect(task_list[1]).to have_content 'first_task'
+          expect(task_list[2]).to have_content 'third_task'
+        end
+      end
+    end
+
+    describe 'Search function' do
+      context 'If you do a fuzzy search by Title' do
+        it 'Only tasks containing the search word will be displayed.' do
+          fill_in 'タイトル', with: 'first'
+          find('#search_task').click
+          expect(page).to have_content 'first_task'
+          expect(page).not_to have_content 'second_task'
+          expect(page).not_to have_content 'third_task'
+        end
+      end
+
+      context 'Search by status' do
+        it 'Only tasks matching the searched status will be displayed' do
+          select '未着手', from: 'search[status]'
+          find('#search_task').click
+          expect(page).to have_content 'first_task'
+          expect(page).not_to have_content 'second_task'
+          expect(page).not_to have_content 'third_task'
+        end
+      end
+
+      context 'Title and search by status' do
+        it 'Only tasks that contain the search word Title and match the status will be displayed' do
+          fill_in 'タイトル', with: 'second'
+          select '着手中', from: 'search[status]'
+          find('#search_task').click
+          expect(page).to have_content 'second_task'
+          expect(page).not_to have_content 'first_task'
+          expect(page).not_to have_content 'third_task'
+        end
       end
     end
   end
