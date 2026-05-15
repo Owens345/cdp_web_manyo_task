5c35efda7c7c481879baf4e6b46e05c0f7c16a02
Owens345 <owensomwanda@gmail.com>
2026-05-15 06:06:30 +0300

    Fix password blank validation on update for edit screens

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,19 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }, allow_blank: false, on: :create
   validates :password, length: { minimum: 6 }, allow_nil: true, on: :update
+  validate :password_not_blank_on_update, on: :update
+
   before_save :downcase_email
   before_destroy :check_admin_count
   before_update :check_admin_update
 
   private
+
+  def password_not_blank_on_update
+    if password == ''
+      errors.add(:password, :too_short, count: 6)
+    end
+  end
 
   def downcase_email
     self.email = email.downcase
